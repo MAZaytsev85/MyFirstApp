38d030b5308e695137c9bbfbd64a327aac2268f4
MAZaytsev85 <streetrsr2007@yandex.ru>
2022-07-04 18:49:40 +0300

    hw4

--- app/src/main/java/com/example/myfirstapp/ui/CalculatorPresenter.java
+++ app/src/main/java/com/example/myfirstapp/ui/CalculatorPresenter.java
@@ -7,18 +7,41 @@ public class CalculatorPresenter {
     private CalculatorView view;
     private Calculator calculator;
 
+    private double argOne;
+    private Double argTwo;
+    private Operator selectedOperator;
+
     public CalculatorPresenter(CalculatorView view, Calculator calculator) {
         this.view = view;
         this.calculator = calculator;
     }
 
-    public void KeyOnePressed() {
-    }
 
     public void onDigitPressed(int digit) {
+
+        if (argTwo == null) {
+
+
+            argOne = argOne * 10 + digit;
+
+            view.showResult(String.valueOf(argOne));
+
+        } else {
+            argTwo = argTwo * 10 + digit;
+        }
     }
 
     public void onOperatorPressed(Operator operator) {
+        if (selectedOperator == null) {
+
+            argOne = calculator.perform(argOne, argTwo, selectedOperator);
+
+            view.showResult(String.valueOf(argOne));
+
+        }
+        argTwo = 0.0;
+
+        selectedOperator = operator;
     }
 
     public void onZapPressed() {
